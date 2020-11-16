e616eec69a5456a6fd5298b80e3a50ea8a4396eb
Matt McElwee <mmcelwee@dmsi.com>
2020-11-16 13:24:52 -0600

    Added form handling styles

--- public/form.js
+++ public/form.js
@@ -1,20 +1,35 @@
 !(async function () {
   document.querySelector('form').addEventListener('submit', async (e) => {
     e.preventDefault();
-    const email = document.getElementById('email').value;
+    const email = document.getElementById('email');
+    email.disabled = true;
 
-    const res = await fetch('/.netlify/functions/signup', {
+    const button = document.querySelector('button[type="submit"]');
+    button.disabled = true;
+    button.innerText = 'Sending...';
+
+    await fetch('/.netlify/functions/signup', {
       method: 'POST',
       body: JSON.stringify({
         data: {
           type: 'email_signup',
           attributes: {
-            email,
+            email: email.value,
           },
         },
       }),
     });
 
-    console.log(res);
+    const form = document.querySelector('form');
+
+    Array.prototype.slice.call(form.children).forEach((el) => {
+      el.style.display = 'none';
+    });
+
+    const p = document.createElement('p');
+    const strong = document.createElement('strong');
+    strong.innerText = `You're in! I'm so excited to read the Scriptures with you!`;
+    p.append(strong);
+    form.append(p);
   });
 })();
